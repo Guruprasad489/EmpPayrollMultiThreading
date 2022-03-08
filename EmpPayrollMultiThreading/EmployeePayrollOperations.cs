d6fbf18da57c61a81c4a5d374be20cae01b0bdba
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-03-09 00:22:39 +0530

    [Guru] Method to add employees to list

---
+++ EmpPayrollMultiThreading/EmployeePayrollOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmpPayrollMultiThreading
+{
+    public class EmployeePayrollOperations
+    {
+        public List<EmployeeDetails> employeePayrollDetailsList = new List<EmployeeDetails>();
+        public void AddEmployeeToPayroll(List<EmployeeDetails> employeePayrollDetailsList)
+        {
+            employeePayrollDetailsList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.AddEmployeePayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+            });
+            Console.WriteLine("No. of employees: "+this.employeePayrollDetailsList.Count);
+            Thread thr = Thread.CurrentThread;
+            Console.WriteLine("Thread: "+ thr.ManagedThreadId);
+        }
+
+        public void AddEmployeePayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailsList.Add(emp);
+        }
+        public int EmployeeCount()
+        {
+            return this.employeePayrollDetailsList.Count;
+        }
+
+        public void AddEmployeeToPayrollWithThread(List<EmployeeDetails> employeePayrollDetailsList)
+        {
+            employeePayrollDetailsList.ForEach(employeeData =>
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                    this.AddEmployeePayroll(employeeData);
+                    Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+                });
+                thread.Start();
+                Console.WriteLine("Thread:"+ thread.ManagedThreadId);
+            });
+        }
+    }
+}
--- EmpPayrollMultiThreading/Program.cs
+++ EmpPayrollMultiThreading/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,29 @@ namespace EmpPayrollMultiThreading
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee payroll using multithreading");
+
+            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 1, EmployeeName = "Guru" });
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 2, EmployeeName = "Guruprasad" });
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 3, EmployeeName = "Vikas" });
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 4, EmployeeName = "Deepak" });
+
+            EmployeePayrollOperations employeePayrollOperations = new EmployeePayrollOperations();
+            Stopwatch stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
+            employeePayrollOperations.AddEmployeeToPayroll(employeeDetails);
+            stopWatch1.Stop();
+            Console.WriteLine("Duration without multi thread: " + stopWatch1.ElapsedMilliseconds);
+            Console.WriteLine("-------------------");
+
+            //Stopwatch stopWatch2 = new Stopwatch();
+            //stopWatch2.Start();
+            //employeePayrollOperations.AddEmployeeToPayrollWithThread(employeeDetails);
+            //stopWatch2.Stop();
+            //Console.WriteLine("Duration with multi thread: " + stopWatch1.ElapsedMilliseconds);
+            //Console.WriteLine("Employee count"+ employeePayrollOperations.EmployeeCount());
+            //Console.WriteLine("-------------------");
+
             Console.ReadLine();
         }
     }
