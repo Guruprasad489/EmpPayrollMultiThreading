451f2a8cfcbbaf68f4b05f59d4eed6f407c1c22a
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-03-08 22:57:27 +0530

    [Guru] Added Welcome message

---
+++ EmpPayrollMultiThreading/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpPayrollMultiThreading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee payroll using multithreading");
+            Console.ReadLine();
+        }
+    }
+}
